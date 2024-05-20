a3d2c00865cf750dd0a5eb19ac2ce865284123d3
Jorge <hello@jhonboy.com>
2024-05-20 22:18:16 +0100

    GITIGNORE UPDATE

--- node_modules/@dotlottie/player-component/dist/dotlottie-player.styles.d.mts
+++
@@ -1,8 +0,0 @@
-import * as lit from 'lit';
-
-/**
- * Copyright 2023 Design Barn Inc.
- */
-declare const _default: lit.CSSResult;
-
-export { _default as default };
--- node_modules/@dotlottie/player-component/dist/dotlottie-player.styles.mjs
+++
@@ -1,4 +0,0 @@
-export { c as default } from './chunk-ODPU3M3Z.mjs';
-import './chunk-ZWH2ESXT.mjs';
-//# sourceMappingURL=out.js.map
-//# sourceMappingURL=dotlottie-player.styles.mjs.map

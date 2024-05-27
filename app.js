56f25e58f81c6783958ef3d710d1a5404533e25e
Jorge <hello@jhonboy.com>
2024-05-27 16:57:53 +0100

    VERCEL CONFIG ANALYTICS

--- app.js
+++ app.js
@@ -11,9 +11,6 @@ import {
 import { handleLocationChange, buttonsRouteInteraction } from './routes.js';
 import { menuButtonResponse } from './scripts/modules/menuUpdates.mjs';
 
-import { inject } from "./node_modules/@vercel/analytics/dist/index.mjs";
-
-inject();
 
 // Definir appContainer en un ámbito accesible
 let appContainer;

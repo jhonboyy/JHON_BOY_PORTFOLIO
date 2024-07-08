a96366307ff99ea42d90ee2b4e805aa6e4a86b24
Jorge <hello@jhonboy.com>
2024-07-08 12:13:24 +0100

    ✨ NEW FEATURE - About/Works menu animations

--- src/scripts/menu-animations.js
+++ src/scripts/menu-animations.js
@@ -6,7 +6,43 @@ export function animateWorksButton() {
   gsap.to(worksButton, {
     css: {
       pointerEvents: 'none',
-      top: 'calc(199px - 100vh)',
+      top: 'calc(202px - 100vh)',
+    },
+    duration: 1,
+    ease: 'power3.out',
+  });
+};
+
+// src/scripts/works-animations.js
+
+export function showMenuImage (){  
+  
+  const projectsSection = document.getElementById('worksImage');
+  projectsSection.style.opacity = 0;
+
+  setTimeout(() => {
+    gsap.to(projectsSection, { opacity: 1, duration: 1 });
+  }, 1000);
+  
+}
+
+export function animateAboutButton() {
+  const worksButton = document.getElementById('worksButton');
+  const aboutButton = document.getElementById('aboutButton');
+
+  gsap.to(worksButton, {
+    css: {
+      pointerEvents: 'none',
+      top: 'calc(150px - 100vh)',
+    },
+    duration: 1,
+    ease: 'power3.out',
+  });
+
+  gsap.to(aboutButton, {
+    css: {
+      pointerEvents: 'none',
+      top: 'calc(201px - 100vh)',
     },
     duration: 1,
     ease: 'power3.out',

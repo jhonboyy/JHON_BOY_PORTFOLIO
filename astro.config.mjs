c76c52967b763aec150e2680121d4afaae725fd0
Jorge <hello@jhonboy.com>
2024-07-08 13:07:57 +0100

    🔧 SETUP - Vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
+// https://astro.build/config
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    port: 3000        
+    port: 3000
   },
   output: 'server',
-  adapter: vercel(),
-
+  adapter: vercel()
 });

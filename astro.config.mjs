cc6486ba3273cdd86de1160b15d7ffb3fd4b46c8
Jorge <hello@jhonboy.com>
2024-07-10 17:50:31 +0100

    🔧 SETUP - Routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,11 @@ import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    port: 3000
+    port: 3000,
   },
   output: 'server',
-  adapter: vercel()
+  adapter: vercel(),
+  outDir: './dist',
+  publicDir: './public',
+  srcDir: './src',
 });

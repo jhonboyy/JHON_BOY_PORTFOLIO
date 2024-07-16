35cc36eb51cb160a91d76f6e8b1fa009612788b1
Jorge <hello@jhonboy.com>
2024-07-16 16:55:36 +0100

    👌 IMPROVE - CSS UI Animations WIP

--- src/scripts/pages-animations.js
+++ src/scripts/pages-animations.js
@@ -1,7 +1,4 @@
-import { gsap } from 'gsap';
-
 // Función Principal para Aplicar Animaciones
-// Apply Animation Function
 export const applyAnimation = (from, to, callback) => {
   const executeCallback = () => {
     if (typeof callback === 'function') {
@@ -13,14 +10,12 @@ export const applyAnimation = (from, to, callback) => {
     animateAboutButton();
     showAboutMenuImage();
     animateAboutSection();
-    document.getElementById('aboutButton').style.pointerEvents = "none";
-    aboutButton.setAttribute('aria-hidden', 'true');
+    document.getElementById('aboutButton').classList.add('no-pointer-events');
     executeCallback();
   } else if (from === 'index' && to === 'works') {
     animateWorksButton();
     showMenuImage();
-    document.getElementById('worksButton').style.pointerEvents = "none";
-    worksButton.setAttribute('aria-hidden', 'true');
+    document.getElementById('worksButton').classList.add('no-pointer-events');
     animateProjectsSection();
     document.getElementById('aboutButton').style.borderTop = "1px solid black";
     executeCallback();
@@ -28,15 +23,13 @@ export const applyAnimation = (from, to, callback) => {
     animateWorksButtonFromAbout();
     showMenuImage();
     animateProjectsSection();
-    document.getElementById('worksButton').style.pointerEvents = "none";
-    worksButton.setAttribute('aria-hidden', 'true');
+    document.getElementById('worksButton').classList.add('no-pointer-events');
     executeCallback();
   } else if (from === 'works' && to === 'about') {
     animateAboutButtonFromWorks();
     showAboutMenuImage();
     animateAboutSection();
-    document.getElementById('aboutButton').style.pointerEvents = "none";
-    aboutButton.setAttribute('aria-hidden', 'true');
+    document.getElementById('aboutButton').classList.add('no-pointer-events');
     executeCallback();
   } else if (from === 'works' && to === 'index') {
     resetWorksPage();
@@ -47,8 +40,7 @@ export const applyAnimation = (from, to, callback) => {
   } else if (from === 'direct' && to === 'works') {
     animateWorksButton();
     showMenuImage();
-    document.getElementById('worksButton').style.pointerEvents = "none";
-    worksButton.setAttribute('aria-hidden', 'true');
+    document.getElementById('worksButton').classList.add('no-pointer-events');
     animateProjectsSection();
     document.getElementById('aboutButton').style.borderTop = "1px solid black";
     executeCallback();
@@ -56,14 +48,12 @@ export const applyAnimation = (from, to, callback) => {
     animateAboutButton();
     showAboutMenuImage();
     animateAboutSection();
-    document.getElementById('aboutButton').style.pointerEvents = "none";
-    aboutButton.setAttribute('aria-hidden', 'true');
+    document.getElementById('aboutButton').classList.add('no-pointer-events');
     executeCallback();
   } else if (from === 'works' && to === 'works') {
     animateWorksButton();
     showMenuImage();
-    document.getElementById('worksButton').style.pointerEvents = "none";
-    worksButton.setAttribute('aria-hidden', 'true');
+    document.getElementById('worksButton').classList.add('no-pointer-events');
     document.getElementById('aboutButton').style.borderTop = "1px solid black";
     animateProjectsSection();
     executeCallback();
@@ -71,119 +61,60 @@ export const applyAnimation = (from, to, callback) => {
     animateAboutButton();
     showAboutMenuImage();
     animateAboutSection();
-    document.getElementById('aboutButton').style.pointerEvents = "none";
-    aboutButton.setAttribute('aria-hidden', 'true');
+    document.getElementById('aboutButton').classList.add('no-pointer-events');
     executeCallback();
   }
 };
 
 // Funciones de Animación
-export function animateWorksButton() {
+function animateWorksButton() {
   const worksButton = document.getElementById('worksButton');
-  gsap.to(worksButton, {
-    css: { top: 'calc(202px - 100vh)' },
-    duration: 1,
-    ease: 'power3.out',
-  });
+  worksButton.classList.add('animate-slide-up');
 }
 
-export function showMenuImage() {
+function showMenuImage() {
   const projectsSection = document.getElementById('menuImage');
-  projectsSection.style.opacity = 0;
-  setTimeout(() => {
-    gsap.to(projectsSection, { opacity: 1, duration: 1 });
-  }, 1000);
+  projectsSection.classList.add('animate-fade-in');
 }
 
-export function animateAboutButton() {
-  const worksButton = document.getElementById('worksButton');
+function animateAboutButton() {
   const aboutButton = document.getElementById('aboutButton');
-  gsap.to(worksButton, {
-    css: { top: 'calc(150px - 100vh)' },
-    duration: 1,
-    ease: 'power3.out',
-  });
-  gsap.to(aboutButton, {
-    css: { top: 'calc(201px - 100vh)' },
-    duration: 1,
-    ease: 'power3.out',
-  });
+  aboutButton.classList.add('animate-slide-up');
 }
 
-export function showAboutMenuImage() {
+function showAboutMenuImage() {
   const aboutImage = document.getElementById('menuImage');
-  aboutImage.style.opacity = 0;
-  setTimeout(() => {
-    gsap.to(aboutImage, { opacity: 1, duration: 1 });
-  }, 1000);
+  aboutImage.classList.add('animate-fade-in');
 }
 
-export function animateProjectsSection() {
+function animateProjectsSection() {
   const projectsSection = document.getElementById('works-content');
-  setTimeout(() => {
-    gsap.to(projectsSection, { opacity: 1, duration: 1 });
-  }, 1000);
+  projectsSection.classList.add('animate-fade-in');
 }
 
-export function animateAboutSection() {
+function animateAboutSection() {
   const aboutSection = document.getElementById('about-content');
-  setTimeout(() => {
-    gsap.to(aboutSection, { opacity: 1, duration: 1 });
-  }, 1000);
+  aboutSection.classList.add('animate-fade-in');
 }
 
-export function animateWorksButtonFromAbout() {
+function animateWorksButtonFromAbout() {
   const worksButton = document.getElementById('worksButton');
   const aboutButton = document.getElementById('aboutButton');
-  
-  aboutButton.style.top = 'calc(200px - 100vh)';
-  worksButton.style.top = 'calc(150px - 100vh)';
-  aboutButton.style.position = 'absolute';
-  aboutButton.style.borderTop = '1px solid black';
-  aboutButton.style.zIndex = '10';
-
-  gsap.to(aboutButton, {
-    css: { 
-      top: '-50px',
-  },
-    duration: 1,
-    ease: 'power3.out',
-  });
-
+  worksButton.classList.add('animate-slide-up');
+  aboutButton.classList.add('animate-slide-up');
 }
 
-export function animateAboutButtonFromWorks() {
+function animateAboutButtonFromWorks() {
   const aboutButton = document.getElementById('aboutButton');
-  const worksButton = document.getElementById('worksButton');
-  worksButton.style.top = 'calc(150px - 100vh)'
-
-  gsap.to(aboutButton, {
-    css: { top: 'calc(201px - 100vh)' },
-    duration: 1,
-    ease: 'power3.out',
-  });
-
+  aboutButton.classList.add('animate-slide-up');
 }
 
-export function resetWorksPage() {
+function resetWorksPage() {
   const worksButton = document.getElementById('worksButton');
-  gsap.to(worksButton, {
-    css: { top: 'initial' },
-    duration: 0.5,
-    ease: 'power3.in',
-  });
+  worksButton.classList.remove('animate-slide-up');
 }
 
-export function resetAboutPage() {
+function resetAboutPage() {
   const aboutButton = document.getElementById('aboutButton');
-  gsap.to(aboutButton, {
-    css: { top: 'initial' },
-    duration: 0.5,
-    ease: 'power3.in',
-  });
-}
-
-export function animateWorksDirect() {
-  const worksButton = document.getElementById('worksButton');
-  gsap.fromTo(worksButton, { opacity: 0 }, { opacity: 1, duration: 1 });
+  aboutButton.classList.remove('animate-slide-up');
 }

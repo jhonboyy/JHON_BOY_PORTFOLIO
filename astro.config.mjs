bdeb9fd259f85761e4048fac331665aa21302093
Jorge <hello@jhonboy.com>
2024-07-18 16:53:59 +0100

    🔧 SETUP - Astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,18 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   server: {
     host: '0.0.0.0',
-    port: 3000,
+    port: 3000
   },
   output: 'server',
   adapter: vercel(),
   outDir: './dist',
   publicDir: './public',
   srcDir: './src',
+  integrations: [sitemap()]
 });
